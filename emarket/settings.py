203cb451891e8b76afcf09a658fe2ecb2bb05cb1
rudyhend@gmail.com <rudyhend@gmail.com>
2023-05-19 15:36:17 +0800

    fix image thumbnail

--- emarket/settings.py
+++ emarket/settings.py
@@ -202,6 +202,7 @@ STATICFILES_DIRS = [
 
 # Media files
 MEDIA_URL = '/media/'
+# MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- emarket/urls.py
+++ emarket/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.apps import apps
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -23,4 +25,4 @@ urlpatterns = [
     # path('dashboard/emarketinghub/', apps.get_app_config('emarketinghub_dashboard').urls),
     path('emarketinghub/', apps.get_app_config('emarketinghub').urls),
     path('', include(apps.get_app_config('oscar').urls[0])),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
